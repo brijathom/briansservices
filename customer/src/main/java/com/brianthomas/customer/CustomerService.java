7588bde391598e10da3d0661947214b3e1da68a3
Brian Thomas <brianjamesthomas@live.com>
2024-05-27 19:11:04 +0100

    Set up the Notification application

--- customer/src/main/java/com/brianthomas/customer/CustomerService.java
+++ customer/src/main/java/com/brianthomas/customer/CustomerService.java
@@ -2,16 +2,17 @@ package com.brianthomas.customer;
 
 import com.brianthomas.clients.fraud.FraudCheckResponse;
 import com.brianthomas.clients.fraud.FraudClient;
+import com.brianthomas.clients.notification.NotificationClient;
+import com.brianthomas.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final NotificationClient notificationClient;
     private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
@@ -31,7 +32,15 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
 
-        // todo: send notification
+        // todo: make it async. i.e add to queue
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Amigoscode...",
+                                customer.getFirstName())
+                )
+        );
 
     }
 }
