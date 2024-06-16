81ff0488d5d7157ae23e823a8dc09c7451b3ae30
Brian Thomas <brianjamesthomas@live.com>
2024-06-16 10:30:21 +0100

    Refactor microservices for Kubernetes

--- customer/src/main/java/com/brianthomas/customer/CustomerService.java
+++ customer/src/main/java/com/brianthomas/customer/CustomerService.java
@@ -3,6 +3,7 @@ package com.brianthomas.customer;
 import com.brianthomas.amqp.RabbitMQMessageProducer;
 import com.brianthomas.clients.fraud.FraudCheckResponse;
 import com.brianthomas.clients.fraud.FraudClient;
+import com.brianthomas.clients.notification.NotificationClient;
 import com.brianthomas.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
